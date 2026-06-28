f01434c88036fe720cf78e1d2cc2f7f17f713a09
patrimato <patricia.mato.miragaya@udc.es>
2026-06-28 13:09:06 +0200

    chore: initial Spring Boot setup on port 5000

---
+++ src/main/java/com/technical/backend_technical_test/BackendTechnicalTestApplication.java
@@ -0,0 +1,13 @@
+package com.technical.backend_technical_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTechnicalTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendTechnicalTestApplication.class, args);
+	}
+
+}
