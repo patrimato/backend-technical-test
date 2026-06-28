5a6c621659bbcae40c75d11a30ad2932508208ee
patrimato <patricia.mato.miragaya@udc.es>
2026-06-28 20:14:47 +0200

    feat: handle errors when product not found

--- src/main/java/com/technical/backend_technical_test/client/ProductClient.java
+++ src/main/java/com/technical/backend_technical_test/client/ProductClient.java
@@ -2,6 +2,7 @@ package com.technical.backend_technical_test.client;
 
 import com.technical.backend_technical_test.model.Product;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
@@ -18,17 +19,25 @@ public class ProductClient {
     }
 
     public List<Integer> getSimilarIds(String productId) {
-        Integer[] ids = restTemplate.getForObject(
-            BASE_URL + "/product/" + productId + "/similarids",
-            Integer[].class
-        );
-        return ids != null ? Arrays.asList(ids) : List.of();
+        try {
+            Integer[] ids = restTemplate.getForObject(
+                BASE_URL + "/product/" + productId + "/similarids",
+                Integer[].class
+            );
+            return ids != null ? Arrays.asList(ids) : List.of();
+        } catch (HttpClientErrorException e) {
+            return List.of();
+        }
     }
 
     public Product getProductDetail(Integer id) {
-        return restTemplate.getForObject(
-            BASE_URL + "/product/" + id,
-            Product.class
-        );
+        try {
+            return restTemplate.getForObject(
+                BASE_URL + "/product/" + id,
+                Product.class
+            );
+        } catch (HttpClientErrorException e) {
+            return null;
+        }
     }
 }
